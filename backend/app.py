3ee416c0d066a064302fadc4413c2174adfdf80d
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2024-07-23 18:37:27 +0530

    HomePath

--- backend/app.py
+++ backend/app.py
@@ -8,16 +8,11 @@ app.secret_key = "your_secret_key_here"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-CORS(app, resources={r"/*":{'origins':"*"}})
-# CORS(app, resources={r'/*':{'origins': 'http://localhost:8080',"allow_headers": "Access-Control-Allow-Origin"}})
+CORS(app)
 
-
-@app.route("/", methods=["GET"])
+@app.route("/home", methods=["GET","POST"])
 def home():
-    return("Hemlo world !")
-
-
+    return jsonify(message="This is Home")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
